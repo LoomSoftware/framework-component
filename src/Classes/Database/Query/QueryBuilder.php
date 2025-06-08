00a796c0bef3ae27f3b00faeb3b15b80af0ef7d4
Daniel Winning <daniel@winningsoftware.co.uk>
2025-06-08 22:09:12 +0100

    Debugging

--- src/Classes/Database/Query/QueryBuilder.php
+++ src/Classes/Database/Query/QueryBuilder.php
@@ -67,6 +67,8 @@ class QueryBuilder
         $queryString .= $this->getInnerJoinQueryStringPartial();
         $queryString .= $this->getWhereQueryStringPartial();
 
+        var_dump($queryString);
+
         return $queryString;
     }
 
